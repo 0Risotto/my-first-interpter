5cc13907fa9ab8f4c2ecb0124a1a062a5c0115c7
0Risotto <anasjallad19@gmail.com>
2024-12-28 07:21:27 -0300

    My third commit

--- src/com/myint/lox/Scanner.java
+++ src/com/myint/lox/Scanner.java
@@ -1,10 +1,32 @@
 package com.myint.lox;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import static com.myint.lox.TokenType.*;
 
 public class Scanner {
+    private static final Map<String, TokenType> keywords;
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
+
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
     private int start = 0;
@@ -39,11 +61,46 @@ public class Scanner {
         case '+' : addToken(PLUS);break;
         case ';' : addToken(SEMICOLON);break;
         case '*' : addToken(STAR);break;
-        
-        default:
-        Lox.error(line, "Unexpected character.");
+        case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
         break;
-
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+        // '/' needs more work becuz comments start with / also
+     case '/':
+     if(match('/')){
+       while(peek() != '\n' && isAtEND()) advance();
+    }
+    else{
+        addToken(SLASH);
+    }
+    break;
+    case ' ':
+    case '\r':
+    case '\t':
+     break;
+    case '\n':
+     line++; break;
+    case '"': string(); break;
+     default:
+        if(isDigit(c)){
+            number();
+        } 
+        else if (isAlpha(c)){
+            identifier();
+        }   
+         else{
+        Lox.error(line, "Unexpected character.");
+        }
+        break;
+        
     }
  }
  private char advance(){
@@ -55,5 +112,59 @@ public class Scanner {
  private void addToken(TokenType type,Object literal){
     String text = source.substring(start,current);
     tokens.add(new Token(type, text , literal , line));
+}
+private boolean match(char expected){
+    if(isAtEND()) return false;
+    if(source.charAt(current) != expected) return false;
+    current++;
+    return true;
+}
+private char peek(){
+    if(isAtEND()) return '\0';
+    return source.charAt(current);
+}
+private void string(){
+    while(peek()!='"'&& !isAtEND()){
+        if(peek() =='\n') line++;
+        advance();
+    }
+    if(isAtEND()){
+        Lox.error(line, "Underminated string");
+        return;
+    }
+    advance();
+    String value = source.substring(start+1, current -1);
+    addToken(STRING, value);
+}
+private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  }
+  private void number(){
+    
+    while(isDigit(peek())) advance();
+   
+  if(peek() == '.' && isDigit(peekNext())){
+    advance();
+    while(isDigit(peek())) advance();
+    }
+    addToken(NUMBER , Double.parseDouble(source.substring(start,current)));
+    }
+ private char peekNext(){
+    if(current +1 >= source.length()) return '\0';
+    return source.charAt(current+1);
+ }
+ private void identifier(){
+    while(isALphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+
+ }
+private boolean isAlpha(char c){
+    return (c>='a'&& c<='z')||(c>='A'&& c<='Z');
+}
+private boolean isALphaNumeric(char c){
+    return isAlpha(c)||isDigit(c);
 }
 }
